832627b44f701d833881e6a1c0226eaaf7ee7468
komal bhoyar <komalbhoyar790@gmail.com>
2023-02-16 22:43:35 +0530

    text doc

---
+++ pro_1.java
@@ -0,0 +1,13 @@
+class pro_1
+{
+public static void main(String[] args)
+{
+int area;
+int parameter;
+int h=7,w=5;
+parameter=2*(h+w);
+System.out.println("parameter og rectangle="+parameter);
+area=w*h;
+System.out.println("area of rectangle="+area);
+}
+}
