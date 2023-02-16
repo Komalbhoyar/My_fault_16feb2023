b506063f65de9e674fb2ab2461f4b464168eb18a
komal bhoyar <komalbhoyar790@gmail.com>
2023-02-16 22:49:24 +0530

    WAP to compute the parameter area if circle with redius of 6 inch

---
+++ pro_2.java
@@ -0,0 +1,11 @@
+class pro_2
+{
+public static void main(String[] args)
+{
+int r=6;
+double parameter=2*3.14*r;
+System.out.println("parameter of the circle="+parameter);
+double area=3.14*r*r;
+System.out.println("area of circle="+area);
+}
+}
