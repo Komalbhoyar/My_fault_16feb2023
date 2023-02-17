96a6eb5de02d1e8584c901e3d55cef208f22ae6b
komal bhoyar <komalbhoyar790@gmail.com>
2023-02-17 05:45:21 +0530

    ass_16feb2023_Q9

---
+++ calculate_distance.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//WAP calculate the distance two points
+class calculate_distance
+{
+	public static void main(String[] args)
+	{
+		int x1,x2,y1,y2;
+		double dis;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter value of point x1");
+		 x1=sc.nextInt();
+		System.out.println("enter value of point y2");
+		 y1=sc.nextInt();
+		System.out.println("enter value of point x1");
+		 x2=sc.nextInt();
+		System.out.println("enter value of point y2");
+		 y2=sc.nextInt();
+
+		 dis=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("calculate the distance two point="+dis);
+	}
+}
