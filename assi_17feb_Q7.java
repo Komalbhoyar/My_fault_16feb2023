f2d850b227aa1080f937075338a9f31140d8550a
komal bhoyar <komalbhoyar790@gmail.com>
2023-02-20 08:19:51 +0530

    read to integer and check multiple or not

---
+++ assi_17feb_Q7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class assi_17feb_Q7
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num%2==0)
+{
+System.out.println("its multiple value");
+}
+else
+{
+System.out.println("it is not multiple value");
+}
+}
+}
