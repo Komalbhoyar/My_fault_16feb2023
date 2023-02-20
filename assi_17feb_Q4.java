bec45ccbdf9ebbf4ecc683496ed4797ecaa2d727
komal bhoyar <komalbhoyar790@gmail.com>
2023-02-20 10:17:02 +0530

    except 5 no & sum of all value betn them

---
+++ assi_17feb_Q4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class assi_17feb_Q4
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the 5 no");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int d=sc.nextInt();
+int e=sc.nextInt();
+int sum=a+b+c+d+e;
+System.out.println("addition of 5 no="+sum);
+}
+}
